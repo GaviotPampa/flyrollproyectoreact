c8805e1623f68cdb527b3efc03c69379270a08b8
Gabriela Carolina Lopez Carrizo <gaviclcarrizo@gmail.com>
2023-03-08 16:47:31 -0300

    agregando filter y category

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,18 +1,33 @@
 import ItemList from "../ItemList/ItemList";
 import products from "../../products/Products";
-import { useState, useEffect } from "react";
+import { useState, useEffect, params } from "react";
 import "./../styles.css";
 
+function getItemFromDataProducts(categoryURL) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let productFilter = products.filter(
+        (item) => item.category === categoryURL
+      );
+      resolve(productFilter);
+    }, 1000);
+  });
+}
+///////////////////////////////////////
 function ItemListContainer({ greeting }) {
   const [products, setproducts] = useState([]);
+
+  const params = productParams();
+  const idCategory = params.idCategory;
   useEffect(() => {
     const getProducts = new Promise((resolve, reject) => {
       setTimeout(() => {
-        resolve(products);
+        resolve(idCategory);
       }, 2000);
     });
     getProducts.then((res) => setproducts(res));
   }, []);
+
   return (
     <>
       <div className="item-list-container">
